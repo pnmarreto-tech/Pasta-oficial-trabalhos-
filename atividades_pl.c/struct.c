5c40f5410b3360c0db8cee33c7be282f6f1c8144
pnmarreto-tech <pnmarreto@gmail.com>
2026-08-24 13:54:19 -0300

    save

---
+++ atividades_pl.c/struct.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+    struct livro{
+        char  nome[30];
+        char editora[20];
+        int ano;
+        float venda;                        // Struct é uma coleção de variáveis referenciados por um nome fornecendo uma maneira de ter informações agrupados//
+    } livro1,livro2;  
+
+    int main(void){
+        return 0;
+    }
