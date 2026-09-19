b1f04cb2ee748820e6ddb166b2db63577922b004
pnmarreto-tech <pnmarreto@gmail.com>
2026-09-18 21:08:48 -0300

    save

--- python.projects.py/main.py
+++ python.projects.py/main.py
@@ -1,3 +1,5 @@
 #Primeiro codigo em Python!!!
 
-print("Hello World on Python")  #a função print serve para exibir as mensagens no painel
+print("Hello World on Python")  #a função print serve para exibir as mensagens no painel;
+help(print) # a função help ajuda a entender melhor o funcionamento da função print e input;
+help(input)
